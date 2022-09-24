842109898df0794bf36fed12163da2254d002be4
kusaanko <39370373+kusaanko@users.noreply.github.com>
2022-09-24 11:09:50 +0900

    Fix some issue to be able to build

--- p5b8/windows/libusb/ddk_make/build_all.bat
+++ p5b8/windows/libusb/ddk_make/build_all.bat
@@ -1,2 +1,2 @@
-cmd.exe /c make.cmd "arch=x86" "outdir=.\x86"
-cmd.exe /c make.cmd "arch=x64" "outdir=.\x64"
+cmd.exe /c make.cmd "arch=x86" "outdir=.\x86" "winddk=C:\WinDDK\7600.16385.1"
+cmd.exe /c make.cmd "arch=x64" "outdir=.\x64" "winddk=C:\WinDDK\7600.16385.1"
